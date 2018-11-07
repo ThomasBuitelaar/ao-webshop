fcadbebc1df81017382211831dd28b20365ae031
Thomas Buitelaar <99041950@mydavinci.nl>
2018-11-07 13:57:07 +0100

    Updated shop/index/blade.php

--- webshop/resources/views/shop/index.blade.php
+++ webshop/resources/views/shop/index.blade.php
@@ -5,5 +5,16 @@
 @endsection
 
 @section('content')
-<h1>it works!</h1>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://i.redd.it/lghg0eop24d01.jpg" alt="..." style="max-height: 150px" class="img-responsive">
+                <div class="caption">
+                    <h3>Shoot the Fish</h3>
+                    <p>There really is nothing to it. Just shoot the fish. You get one chance and if you miss you need to pay again. Simple as that.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Shoot</a> <a href="#" class="btn btn-default" role="button">Dont shoot</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
