9fec9bacaad467e38c4590876a3189fc21853e50
Thomas Buitelaar <99041950@mydavinci.nl>
2018-12-07 14:29:58 +0100

    Filled Article seeder

--- webshop/database/seeds/ArticleTableSeeder.php
+++ webshop/database/seeds/ArticleTableSeeder.php
@@ -11,7 +11,38 @@ class ArticleTableSeeder extends Seeder
      */
     public function run()
     {
-        $article = new \App\Article();
-        $article->
+        DB::table('articles')->insert([
+            [
+                'name' => "Fish",
+                'imagePath' => "img1.png",
+                'price' => "5.99",
+                'description' => "very nice animal to either shoot or eat.",
+            ],
+            [
+                'name' => "Really cool gun",
+                'imagePath' => "img2.png",
+                'price' => "500",
+                'description' => "you can shoot fishes or something.",
+            ],
+            [
+                'name' => "Buisness suit",
+                'imagePath' => "img3.png",
+                'price' => "149.99",
+                'description' => "Really good atire to wear while shooting fish.",
+            ],
+            [
+                'name' => "Funflower",
+                'imagePath' => "img4.png",
+                'price' => "3.99",
+                'description' => "Some cool flower that is named after the hot ball in the sky.",
+            ],
+            [
+                'name' => "Cat",
+                'imagePath' => "img5.png",
+                'price' => "0",
+                'description' => "Cats are the supperior pet.d",
+            ]
+        ]);
+    
     }
 }
